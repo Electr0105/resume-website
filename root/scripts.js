a2272a161a9af8b34f434405566012b1894b58ee
Jackson <jackson.gleeson@amtek.com.au>
2025-05-28 12:08:35 +1000

    test

--- root/scripts.js
+++ root/scripts.js
@@ -1,3 +1,10 @@
-function countVisitors() {
-  console.log("123");
-}
+async function countVisitors(){
+  try{
+    const response = await fetch('https://resume-functions.azurewebsites.net/api/visitorcount?');
+    if (!response.ok) throw new Error("Network response was not ok");
+    const quoteElement = document.getElementById("visitorcount");
+    quoteElement.textContent = "Total number of site visitors :" + response;
+  } catch (error) {
+    quoteElement.textContent = "FAILED";
+  }
+  }
