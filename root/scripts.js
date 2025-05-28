2a5ad0fc5e7f7860bfdef11525e68deba898ed01
Jackson <jackson.gleeson@amtek.com.au>
2025-05-28 13:10:53 +1000

    Site visitor count fix

--- root/scripts.js
+++ root/scripts.js
@@ -1,10 +1,5 @@
-async function countVisitors(){
-  try{
-    const response = await fetch('https://resume-functions.azurewebsites.net/api/visitorcount?');
-    if (!response.ok) throw new Error("Network response was not ok");
-    const quoteElement = document.getElementById("visitorcount");
-    quoteElement.textContent = "Total number of site visitors :" + response;
-  } catch (error) {
-    quoteElement.textContent = "FAILED";
-  }
-  }
+const countVisitors = async () => {
+  const response = await fetch('https://resume-functions.azurewebsites.net/api/visitorcount?');
+  const myJson = await response.json(); //extract JSON from the http response
+  const quoteElement = document.getElementById("visitorcount").textContent = "Number of site visitors: " + myJson;
+}
